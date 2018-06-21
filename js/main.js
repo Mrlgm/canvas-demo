1a6ee15959eb91d9c4b9846ec4456406419beb66
Mrlgm <877205619@qq.com>
2018-06-21 17:06:02 +0800

    增加颜色选择下载功能

--- js/main.js
+++ js/main.js
@@ -8,19 +8,67 @@ listenToUser(canvas)
 /*********************/
 
 var eraserEnabled = false
-var eraser = document.getElementById('eraser')
-var brush = document.getElementById('brush')
-var actions = document.getElementById('actions')
+
+pen.onclick = function () {
+    eraserEnabled = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
 
 eraser.onclick = function () {
     eraserEnabled = true
-    actions.className = 'actions x'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
 }
 
-brush.onclick = function () {
-    eraserEnabled = false
-    actions.className = 'actions'
+clear.onclick = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
+download.onclick = function(){
+    var url = canvas.toDataURL("image/png")
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = 'painting'
+    a.target = '_blank'
+    a.click()
+}
+
+black.onclick = function(){
+    context.fillStyle = 'black'
+    context.strokeStyle = 'black'
+    black.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+
+red.onclick = function(){
+    context.fillStyle = 'red'
+    context.strokeStyle = 'red'
+    black.classList.remove('active')
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+
+green.onclick = function(){
+    context.fillStyle = 'green'
+    context.strokeStyle = 'green'
+    black.classList.remove('active')
+    red.classList.remove('active')
+    green.classList.add('active')
+    blue.classList.remove('active')
+}
+
+blue.onclick = function(){
+    context.fillStyle = 'blue'
+    context.strokeStyle = 'blue'
+    black.classList.remove('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.add('active')
 }
 
 function setWidthAndHeight() {
@@ -33,14 +81,12 @@ function setWidthAndHeight() {
 
 function drawCircle(x, y, radius) {
     context.beginPath()
-    context.fillStyle = "black"
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill()
 }
 
 function drawLiine(x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = "black"
     context.moveTo(x1, y1) //起点
     context.lineWidth = 5
     context.lineTo(x2, y2) //终点
